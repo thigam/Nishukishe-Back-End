c150311b83820273924f17ba16e9b3ff44560be2
Thigs <matthewthiga72@gmail.com>
2026-06-14 01:26:03 +0200

    perf: reduce WalkRouter OSRM timeout to 2s to protect against connection hangs

--- app/Services/WalkRouter.php
+++ app/Services/WalkRouter.php
@@ -28,7 +28,7 @@ class WalkRouter
             $url = "{$this->base}/{$coords}?overview=full&geometries=geojson";
 
             try {
-                $res = Http::timeout(8)->acceptJson()->get($url);
+                $res = Http::timeout(2)->acceptJson()->get($url);
 
                 if ($res->ok()) {
                     $route = $res->json('routes.0');
